d472767b0a9aaf29c37a2bc434cc1865f9ef62e5
usr2 <pain63tartar@gmail.com>
2026-08-05 00:08:45 +0600

    bootstrap setup done

---
+++ 3_Bootstrap/learning-bootstrap/src/App.jsx
@@ -0,0 +1,23 @@
+// npm i bootstrap@5.3.8 -> run in terminal to download dependencies;
+// import 'bootstrap/dist/css/bootstrap.min.css; -> import this directory into main.jsx;
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+      <button type="button" class="btn btn-success">Success</button>
+      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn btn-warning">Warning</button>
+      <button type="button" class="btn btn-info">Info</button>
+      <button type="button" class="btn btn-light">Light</button>
+      <button type="button" class="btn btn-dark">Dark</button>
+      <button type="button" class="btn btn-link">Link</button>
+    </div>
+  )
+}
+
+export default App
